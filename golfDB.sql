c297e5d0f6881dcacaf6fe656f3387583f541633
KimRoys <85109206+KimRoys@users.noreply.github.com>
2021-11-30 10:46:15 +0100

    Corrected updates

--- golfDB.sql
+++ golfDB.sql
@@ -36,11 +36,11 @@ create table rainType(
 -- egen tabell för om en tävling har regn, med vindstyrka
 drop table if exists rain;
 create table rain(
-    rainId int unsigned auto_increment primary key,
     rainTypeId int unsigned,
     windStrength int unsigned not null,
     occurance time not null,
     compname varchar(25) not null,
+    primary key (compname, rainTypeId),
     foreign key (rainTypeId) references rainType(rainTypeId)
 		on update cascade on delete restrict,
     foreign key (compname) references competition(compname)
@@ -51,11 +51,11 @@ create table rain(
 -- tänkte först använda modelNr, men blir skumt med ett modelNr per storlek
 drop table if exists jacket;
 create table jacket(
-	jacketId int unsigned primary key,
 	model varchar(30),
 	size varchar(5) not null,
 	material varchar(30) not null,
     playerpnr varchar(13) not null,
+    primary key (model, playerpnr),
 	foreign key(playerpnr) references player(playerpnr) 
         on update cascade on delete restrict
 )engine=InnoDB;
@@ -68,10 +68,11 @@ create table golfclubconst(
 
 drop table if exists golfclub;
 create table golfclub(
-	nr int unsigned primary key,
+	nr int unsigned,
 	material varchar(15),
     playerpnr char(13),
     serialNr int unsigned,
+    primary key (nr, playerpnr, serialNr),
 	foreign key(playerpnr) references player(playerpnr)
 		on update cascade on delete restrict,
     foreign key (serialNr) references golfclubconst(serialNr)
